a29dfc04b4948bf67588d534433e39db65db56f9
silmidina <silmirhmdn08@gmail.com>
2025-05-29 16:29:25 +0700

    TugasDay4-Cypress-BDD

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,35 @@
+// const { defineConfig } = require("cypress");
+
+// module.exports = defineConfig({
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//   },
+// });
+
 const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    async setupNodeEvents(on, config) {
+      // Tambahkan plugin Cucumber
+      await addCucumberPreprocessorPlugin(on, config);
+
+      // Gunakan bundler esbuild untuk mendukung fitur Cucumber
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+
+      return config;
     },
+    specPattern: "cypress/e2e/**/*.feature", // Menargetkan file feature
+    baseUrl: "https://www.saucedemo.com",
   },
 });
